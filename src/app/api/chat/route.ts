8077f2d169f6e67555bbfb469c839c3033c0f21e
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-02-08 22:36:57 +0530

    basic chatbot completed

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,31 @@
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import OpenAI from "openai";
+import { ChatCompletionMessageParam } from "ai/prompts";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const messages = body.messages;
+
+    const openai = new OpenAI();
+
+    const systemMessage: ChatCompletionMessageParam = {
+      role: "system",
+      content:
+        "You are a sarcasm bot. You answer all user questions in a sarcastic way.",
+    };
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [systemMessage, ...messages],
+    });
+
+    const stream = OpenAIStream(response);
+
+    return new StreamingTextResponse(stream);
+  } catch (error) {
+    console.log("error: ", error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
