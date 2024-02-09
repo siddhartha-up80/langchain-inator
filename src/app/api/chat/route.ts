25f864ea723dd93f703fb3871b3a6deba46cfd0a
Siddhartha Singh <84330754+siddhartha-up80@users.noreply.github.com>
2024-02-09 19:31:45 +0530

    basic chatbot with langchain done

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,27 +1,37 @@
-import { OpenAIStream, StreamingTextResponse } from "ai";
-import OpenAI from "openai";
+import { LangChainStream, OpenAIStream, StreamingTextResponse } from "ai";
 import { ChatCompletionMessageParam } from "ai/prompts";
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import OpenAI from "openai";
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
     const messages = body.messages;
 
-    const openai = new OpenAI();
+    const currentMessageContent = messages[messages.length - 1].content;
 
-    const systemMessage: ChatCompletionMessageParam = {
-      role: "system",
-      content:
-        "You are a sarcasm bot. You answer all user questions in a sarcastic way.",
-    };
+    const { stream, handlers } = LangChainStream();
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      stream: true,
-      messages: [systemMessage, ...messages],
+    const chatModel = new ChatOpenAI({
+      modelName: "gpt-3.5-turbo",
+      streaming: true,
+      callbacks: [handlers],
     });
 
-    const stream = OpenAIStream(response);
+    const prompt = ChatPromptTemplate.fromMessages([
+      [
+        "system",
+        "You are a sarcasm bot. You answer all user questions in a sarcastic way.",
+      ],
+      ["user", "{input}"],
+    ]);
+
+    const chain = prompt.pipe(chatModel);
+
+    chain.invoke({
+      input: currentMessageContent,
+    });
 
     return new StreamingTextResponse(stream);
   } catch (error) {
